e52091defd80bce04250e48da29599ad85f1dbce
Estudiantes <Estudiantes@Labing50424.udistrital.edu.co>
2018-02-09 11:47:26 -0500

    tercer commit

---
+++ Micalculadora/src/Principal.java
@@ -0,0 +1,27 @@
+
+import micalculadora.Calculadora;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Principal {
+    
+   public static void main( String[] args ) {
+       //Crear un objeto de la clase Calculadora
+       
+       Calculadora micalculadora; //definicion
+       micalculadora=new Calculadora();//construir objeto
+       
+        
+    }
+
+    }
+    
+
